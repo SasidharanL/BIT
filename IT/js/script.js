f8316b4f78cbbfe1514c2d71b8a324c3ac0e4c28
SasidharanL <sasi9865dharan@gmail.com>
2021-03-05 14:52:18 +0530

    Update

--- IT/js/script.js
+++ IT/js/script.js
@@ -1,35 +1,7 @@
-let input = document.getElementById("filter-input");
-let t_body = document.getElementById("table-body");
-let tr = t_body.getElementsByTagName("tr");
+// sticky navigation
 
-input.addEventListener("keyup", () => {
-    let filter = input.value.toUpperCase();
+let header = document.querySelector('header');
 
-    for (var i = 0; i < tr.length; i++) {
-        if (tr[i].textContent.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-        }
-        else {
-            tr[i].style.display = "none";
-        }
-    }
-    
-    check_shown();
+window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
-
-function check_shown() {
-    let shown_count = 0;       
-    let found = document.querySelector(".filter_info");
-    
-    for (let i = 0; i < tr.length; i++) {
-        if (tr[i].style.display == "") {
-            shown_count++;
-        }    
-    }
-    if (shown_count == 0) {
-        found.classList.add("show");
-    }    
-    else{
-        found.classList.remove("show");
-    }
-}
