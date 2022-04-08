992c80f58203ecfa345be3dba2b2e8d81d192e49
SasidharanL <sasi9865dharan@gmail.com>
2022-04-08 19:29:49 +0530

    Commit

--- NCIIT/js/script.js
+++ NCIIT/js/script.js
@@ -2,7 +2,7 @@ let menu_btn = document.querySelector(".menu-btn");
 let nav = document.querySelector(".nav-list");
 let menu_icon = document.querySelector(".fa-bars");
 
-menu_btn.addEventListener("click", (event)=>{
+menu_btn.addEventListener("click", (event) => {
     menu_btn.classList.toggle("active");
     nav.classList.toggle("open");
     menu_icon.classList.toggle("fa-xmark");
