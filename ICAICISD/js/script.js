196c65eb31e82dc4e0345990b2eeadcd2889fccf
SasidharanL <sasi9865dharan@gmail.com>
2024-09-27 00:05:30 +0530

    Delete ICAICISD directory

--- ICAICISD/js/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-    const preloader = document.querySelector('.loader-wrapper');
-    preloader.classList.add('loaded');
-    document.body.style.overflowY = "scroll";
-});
-
-
